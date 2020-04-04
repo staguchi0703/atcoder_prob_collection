a12802f1841d3a39f2c63def31c307f18958ffc3
sho Taguchi <s.taguchi0703@gmail.com>
2020-04-04 20:59:24 +0900

    ABC123

--- ABC123C/resolve.py
+++ ABC123C/resolve.py
@@ -3,32 +3,13 @@ def resolve():
     '''
     code here
     '''
+    import math
     N = int(input())
     trans_list = [int(input()) for _ in range(5)]
 
-    memo = [0 for _ in range(6)]
-    memo[0] = N
-
-
-    def trans(i, num):
-        if memo[i] > num:
-            memo[i] -= num
-            memo[i+1] += num
-        else:
-            memo[i+1] += memo[i]
-            memo[i] = 0
-            
-
-
-    res = 0
-    while memo[5] != N:
-        for i in reversed(range(5)):
-            # print(i, trans_list[i])
-            if memo[i] > 0:
-                trans(i, trans_list[i])
-        res += 1
-        # print(memo)
-    print(res)
+    min_tp = min(trans_list)
+    
+    print(math.ceil(N/min_tp) + 4)
 
 if __name__ == "__main__":
     resolve()
